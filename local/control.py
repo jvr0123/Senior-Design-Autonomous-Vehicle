c8c1dd3585dc6262daae75940ca4388a18043cf9
Jorge Ragde <ragdejorge@gmail.com>
2021-02-08 19:48:55 -0800

    Made movement controls thread-safe, added remote capabilities

--- local/control.py
+++ local/control.py
@@ -1,117 +1,68 @@
-import RPi.GPIO as gpio
-import time
 import cv2
+import time
+import gpiozero as gpio
+import pigpio
 
-#for our servo, a duty of 6 for the input wave sends the servo to the middle. the relationship between the duty and
-#angle is duty = 2+angle/15
-duty = 6
+pi = pigpio.pi(host='192.168.1.204')
 
 
 def init(flag):
-    gpio.setmode(gpio.BCM)
     # motors
-    gpio.setup(17, gpio.OUT)
-    gpio.setup(22, gpio.OUT)
-    gpio.setup(23, gpio.OUT)
-    gpio.setup(24, gpio.OUT)
+    pi.set_mode(17, pigpio.OUTPUT)
+    pi.set_mode(22, pigpio.OUTPUT)
+    pi.set_mode(23, pigpio.OUTPUT)
+    pi.set_mode(24, pigpio.OUTPUT)
 
     # servo
-    #the bool flag decides whether we return none or an object for the servos
+    # the bool flag decides whether we return none or an object for the servos
     if flag:
-        gpio.setup(4, gpio.OUT)
-        servo1 = gpio.PWM(4, 50)
-        servo1.start(0)
-        return servo1
-    else:
-        return
-
+        pi.set_mode(4, pigpio.OUTPUT)
+        pi.set_PWM_frequency(4, 50)
+def cleargpio():
+    pi.write(17, 0)
+    pi.write(22, 0)
+    pi.write(23, 0)
+    pi.write(24, 0)
 
 def forward(sec):
     init(False)
-    gpio.output(17, True)
-    gpio.output(22, False)
-    gpio.output(23, True)
-    gpio.output(24, False)
+    pi.write(17, 1)
+    pi.write(22, 0)
+    pi.write(23, 1)
+    pi.write(24, 0)
     time.sleep(sec)
-    gpio.cleanup()
+    cleargpio()
 
 
 def reverse(sec):
     init(False)
-    gpio.output(17, False)
-    gpio.output(22, True)
-    gpio.output(23, False)
-    gpio.output(24, True)
+    pi.write(17, 0)
+    pi.write(22, 1)
+    pi.write(23, 0)
+    pi.write(24, 1)
     time.sleep(sec)
-    gpio.cleanup()
+    cleargpio()
 
-
-def turnRight(sec):
-    servo1 = init(True)
-    global duty
-    if duty >= 2.6:
-        duty = duty - 2.6
-    servo1.ChangeDutyCycle(duty)
-    time.sleep(sec)
-    servo1.stop()
-    gpio.cleanup()
-
-
-def turnLeft(sec):
-    servo1 = init(True)
-    global duty
-    if duty <= 9.4:
-        duty = duty + 2.6
-    servo1.ChangeDutyCycle(duty)
-    time.sleep(sec)
-    servo1.stop()
-    gpio.cleanup()
-
-
-def center(sec):
-    servo1 = init(True)
-    global duty
-    duty = 6
-    servo1.ChangeDutyCycle(duty)
-    time.sleep(sec)
-    servo1.stop()
-    gpio.cleanup()
-
-
-# start video capture
 
 cap = cv2.VideoCapture(0)
-
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
 
 while (True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    # flips camera as required by our system
+    #flips camera as required by our system
     frame = cv2.flip(frame, 0)
 
-    out.write(frame)
     # Display the resulting frame
-    cv2.imshow('frame', frame)
+    cv2.imshow('frame',frame)
 
     keyPress = cv2.waitKey(20)
-    #these values are picked experimentally
     if keyPress & 0xFF == ord('w'):
         forward(.03)
     elif keyPress & 0xFF == ord('s'):
         reverse(.03)
-    elif keyPress & 0xFF == ord('a'):
-        turnLeft(.1)
-    elif keyPress & 0xFF == ord('d'):
-        turnRight(.1)
-    elif keyPress & 0xFF == ord('t'):
-        center(.1)
     elif keyPress & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
 cap.release()
-out.release()
 cv2.destroyAllWindows()
